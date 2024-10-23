4e4e3919c34f531d695b16ef30b817224f9d5df2
Chaya <cheyenne.xyn@gmail.com>
2024-10-23 21:49:52 +0200

    html and so on

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,43 @@
-use clap::Parser;
 use wasm_bindgen::prelude::*;
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+use web_sys::window;
+use wasm_bindgen::JsCast;
+use std::fmt::Write;
 
 #[wasm_bindgen]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    message: String,
-}
-#[wasm_bindgen]
-fn frame(message: String, decorator: char) -> std::string::String { 
-    // Count Length of Message 
-    let length = message.len() + 2;
-    // Return a decorator repeated at message length
-    let mut border = String::from("");
+pub fn generate_ascii_bubble(message: &str) -> String {
+    let mut bubble = String::new();
+    let length = message.len() + 4; // Adds padding around message
     
-    for _n in 1..length {
-        border.push(decorator);
-    }
-    return border 
-} 
-#[wasm_bindgen] 
-fn main() {
-    let args = Args::parse();
-    println!(" {} ", frame(args.message.clone(), '_'));
-    println!("< {} >", args.message);
-    println!(" {} ",frame(args.message, '-'));
-    println!("   |\\---/|
-   | ,_, |
-    \\_`_/-..----.
- ___/ `   ' ,//+ \\  sk
-(__...'   __\\    |`.___.';
-  (_,...'(_,.`__)/'.....+");
+    // Top border
+    let _ = writeln!(bubble, " {}", "-".repeat(length));
+    // Message with padding
+    let _ = writeln!(bubble, "< {} >", message);
+    // Bottom border
+    let _ = writeln!(bubble, " {}", "-".repeat(length));
+    
+    bubble
+}
+
+#[wasm_bindgen]
+pub fn countdown_then_show_message(message: String) {
+    let window = window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    //closure for the countdown
+    let closure = Closure::wrap(Box::new(move || {
+        // After countdown, show the ASCII bubble
+        let ascii_bubble = generate_ascii_bubble(&message);
+        body.set_inner_html(&format!("<pre>{}</pre>", ascii_bubble));
+    }) as Box<dyn FnMut()>);
+
+    // `unchecked_ref()` to convert closure to a functio reference expected by `setTimeout`
+    window
+        .set_timeout_with_callback_and_timeout_and_arguments_0(
+            closure.as_ref().unchecked_ref(), // Cast closure to `&Function`
+            3000, // 3 seconds timeout
+        )
+        .expect("Should register `setTimeout` OK");
+
+    closure.forget(); // prevent premature closure deallocation 
 }
